df6447aa3217e574c5c7d7d6e4ac239f1ff21149
Ujjwal Jha <ujjwaljha1225@gmail.com>
2024-10-02 05:43:46 +0545

    Generated custom data

---
+++ client/src/data/categoriesData.js
@@ -0,0 +1,48 @@
+export const categoriesData = {
+  Men: {
+    Topwear: [
+      "T-shirts",
+      "Casual shirts",
+      "Formal shirts",
+      "Sweaters",
+      "Jackets",
+      "Blazers",
+    ],
+    Bottomwear: ["Jeans", "Shorts", "Cotton pants", "Trousers"],
+    Footwear: ["Sports shoes", "Formal shoes", "Slippers"],
+    Sportswear: ["Tracksuit", "Swimsuit"],
+  },
+  Women: {
+    Topwear: ["T-shirts", "Tops", "Sweaters", "Jackets", "Blazers"],
+    Bottomwear: ["Jeans", "Shorts", "Trousers", "Leggings", "Skirts"],
+    Footwear: ["Sports shoes", "Formal shoes", "Heels", "Sandals"],
+    Sportswear: ["Tracksuit", "Swimsuit"],
+    "Fusion wear": ["Kurthas", "Sari", "Lehenga"],
+  },
+  Kids: {
+    Boys: ["T-shirts", "Jeans", "Sweaters", "Jackets", "Shorts"],
+    Girls: ["Tops", "Kurtha Sets", "Lehenga"],
+    Footwear: ["School shoes", "Sports shoes", "Flipflops", "Socks"],
+    "Kids Accessories": ["Bags", "Watches", "Caps", "Sunglasses"],
+  },
+  // "Home & Living": {
+  //   Furniture: ["Sofas", "Beds", "Tables", "Chairs"],
+  //   "Home Decor": ["Curtains", "Rugs", "Wall Art", "Cushions"],
+  //   Kitchenware: ["Cookware", "Cutlery", "Utensils", "Appliances"],
+  // },
+  Beauty: {
+    Skincare: ["Cleansers", "Moisturizers", "Serums", "Sunscreen"],
+    Makeup: ["Foundation", "Lipstick", "Eyeshadow", "Mascara"],
+    Haircare: ["Shampoo", "Conditioner", "Hair Oil", "Styling Products"],
+  },
+  // Studio: {
+  //   Photography: ["Cameras", "Lenses", "Tripods", "Lighting Equipment"],
+  //   "Art Supplies": ["Paints", "Brushes", "Canvas", "Easels"],
+  //   "Music Production": [
+  //     "Microphones",
+  //     "Mixers",
+  //     "Studio Monitors",
+  //     "MIDI Controllers",
+  //   ],
+  // },
+};
---
+++ client/src/data/imageData.js
@@ -0,0 +1,13 @@
+import coupon from "../images/coupon.png";
+import logo from "../images/logo.svg";
+
+export const images = [
+  {
+    name: "coupon",
+    image: coupon,
+  },
+  {
+    name: "logo",
+    image: logo,
+  },
+];
